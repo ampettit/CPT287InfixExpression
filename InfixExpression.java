e546dcb357594f8f19b2518fe4ec6d1d09b70f99
8i11y79 <66650246+8i11y79@users.noreply.github.com>
2020-06-28 18:54:10 -0500

    Update InfixExpression.java

--- InfixExpression.java
+++ InfixExpression.java
@@ -1,16 +1,25 @@
 package infix;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Parser {
 	
 	public static String Parse (String problem) {
 		String answer = "";
+		
+		// Make a stack from the problem
+        	Stack<Character> problemStack = new Stack<>();
+        	for (int i = 0; i < problem.length(); i++) { 
+        		problemStack.push(problem.charAt(i));}
+        	
 		return answer;
 	}
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		
+		// Get problem from user
 		System.out.println ("Enter your problem:");
 		String problem = scan.next();
 		
